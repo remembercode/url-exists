ecce43e7a4a108acf8729f65d5e77cc294b2b035
WangBing <wangbing1985@163.com>
2017-09-27 14:33:02 +0800

    add "strictSSL: false " to request option

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var request = require('request');
 var dateTime = require('date-time');
 
 function urlExists(url, cb) {
-  request({ url: url, method: 'HEAD' }, function(err, res) {
-    if (err) return cb(err);
-    cb(null, {
-		url: url,
-		statusCode: res.statusCode,
-		time: dateTime({showMilliseconds: true}),
-		alive: /[4-5]\d\d/.test(res.statusCode) === false
+	request({ url: url, strictSSL: false, method: 'HEAD' }, function(err, res) {
+		if (err) return cb(err);
+		cb(null, {
+			url: url,
+			statusCode: res.statusCode,
+			time: dateTime({showMilliseconds: true}),
+			alive: /[4-5]\d\d/.test(res.statusCode) === false
+		});
 	});
-  });
 }
 
 module.exports = urlExists;
