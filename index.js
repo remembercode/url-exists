ed845afaa960dd5bedad19d15f5134e431941134
WangBing <wangbing1985@163.com>
2017-09-27 13:09:14 +0800

    change npm test target url from google to baidu, change return from sample Boolean to Json Object {url: url, statusCode: res.statusCOde, alive: Boolean}

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var request = require('request');
 
 function urlExists(url, cb) {
   request({ url: url, method: 'HEAD' }, function(err, res) {
-    if (err) return cb(null, false);
-    cb(null, /[4-5]\d\d/.test(res.statusCode) === false);
+    if (err) return cb(err);
+    cb(null, {
+		url: url,
+		statusCode: res.statusCode,
+		alive: /[4-5]\d\d/.test(res.statusCode) === false
+	});
   });
 }
 
--- test/index.js
+++ test/index.js
@@ -2,17 +2,17 @@ var assert = require('assert');
 
 var urlExists = require('..');
 
-urlExists('https://www.google.com', function(err, exists) {
-  assert(exists === true);
+urlExists('https://www.baidu.com', function(err, exists) {
+  assert(exists.alive === true);
 
-  urlExists('https://google.com', function(err, exists) {
-    assert(exists === true);
+  urlExists('https://baidu.com', function(err, exists) {
+    assert(exists.alive === true);
 
-    urlExists('http://www.google.com', function(err, exists) {
-      assert(exists === true);
+    urlExists('http://www.baidu.com', function(err, exists) {
+      assert(exists.alive === true);
 
       urlExists('https://www.asdflkasdfljalsfdjasfdljklsjafasdfljjkasfdsafdljfdsaljakljsdljksafasfdlk.lasjkd', function(err, exists) {
-        assert(exists === false);
+        assert(exists.alive === false);
 
         console.log('All tests pass!');
         process.exit(0);
